edcb85af341998ebfe0ad5399d22bc07fff1c59a
Dipakkandel-2004 <137183953+Dipakkandel-2004@users.noreply.github.com>
2025-01-10 15:29:08 +0545

    Update Linear_probing_Hashing.py

--- Linear_probing_Hashing.py
+++ Linear_probing_Hashing.py
@@ -27,10 +27,25 @@ class dict:
         return abs(hash(key))%self.size
     def rehash(self,old_hash):
         return (old_hash+1)%self.size
+    def get(self,key):
+        start=self.hash_func(key)
+        curr=start
+        if self.slots[start]==key:
+            return self.data[start]
+
+        while self.slots[curr] is not None:
+            curr=self.rehash(curr)
+            if self.slots[curr]==key:
+                return self.data[curr]
+            if curr==start:
+                return "NOt FOUND (list is full)"
+        return "NOT FOUND(none is found) "
+    def __getitem__(self, item):
+        return self.get(item)
 d1=dict(5)
 d1.put(5,3)
 d1.put(10,11)
 d1.put(15,11)
 d1.put(12,11)
 print(d1.slots)
-print(d1.data)
+print(d1[182])
