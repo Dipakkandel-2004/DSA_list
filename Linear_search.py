f71bf75084fc4f79a2c9fd6d55f3f109f8ce8152
Dipakkandel-2004 <137183953+Dipakkandel-2004@users.noreply.github.com>
2025-01-11 23:03:32 +0545

    Create Linear_search.py

---
+++ Linear_search.py
@@ -0,0 +1,7 @@
+list=[12,23,4,3,23,5,65,23,64,5,64]
+def linear_search(list,quary):
+    for i in range(len(list)):
+        if list[i]==quary:
+            print(i)
+    return
+linear_search(list,52
